4472baa878e912b9ff82e28e575342552837ac4b
Sebastian Johansson <Xenose@live.com>
2018-05-23 21:02:40 +0900

    Just line stuff

--- Tetris/Source.cpp
+++ Tetris/Source.cpp
@@ -1,8 +1,7 @@
 ﻿#include<stdint.h>
+#include<stdlib.h>
 #include<stdio.h>
 #include<conio.h>
-#include<Windows.h>
-
 
 #define GAME_WIDTH 31
 
@@ -10,37 +9,50 @@
 #define LINE 0, 2, 1, 2, 2, 2, 3, 2
 #define CUBE 0, 2, 1, 2, 0, 1, 1, 1
 #define S_BLOCK 2, 2, 1, 2, 2, 1, 3, 1
+#define Z_BLOCK 3, 2, 4, 2, 2, 1, 3, 1
+#define L_BLOCK 2, 1, 2, 2, 2, 3, 3, 3
+#define RL_BLOCK 2, 1, 2, 2, 2, 3, 1, 3
 
 uint32_t data[26];
 uint16_t score = 0;
 
+#define posX1 pos[0]
+#define posX2 pos[1]
+#define posX3 pos[2]
+#define posX4 pos[3]
+
+#define posY1 pos[4]
+#define posY2 pos[5]
+#define posY3 pos[6]
+#define posY4 pos[7]
+
+char index = 0;
+char a = '0';
+
 struct Block
 {
-	char x1;
-	char y1;
+	char* pos = (char*)malloc(8);
 
-	char x2;
-	char y2;
-	
-	char x3;
-	char y3;
-
-	char x4;
-	char y4;
-
-	Block(){}
+	~Block()
+	{
+		if (pos != NULL) 
+		{
+			free(pos);
+			pos = NULL;
+		}
+	}
 
 	void SpawnBlock(const char& _x1, const char& _y1, const char& _x2, const char& _y2, const char& _x3, const char& _y3, const char& _x4, const char& _y4)
 	{
-		x1 = _x1;
-		x2 = _x2;
-		x3 = _x3;
-		x4 = _x4;
+		posX1 = _x1;
+		posX2 = _x2;
+		posX3 = _x3;
+		posX4 = _x4;
 
-		y1 = _y1;
-		y2 = _y2;
-		y3 = _y3;
-		y4 = _y4;
+		posY1 = _y1;
+		posY2 = _y2;
+		posY3 = _y3;
+		posY4 = _y4;
 
 		data[_y1] |= 0x1 << _x1;
 		data[_y2] |= 0x1 << _x2;
@@ -48,112 +60,147 @@ struct Block
 		data[_y4] |= 0x1 << _x4;
 	}
 
+	void SpawnRandomBlock()
+	{
+
+		srand(index);
+		switch (rand() % 7)
+		{
+		case 0:
+			SpawnBlock(LINE);
+			break;
+		case 1:
+			SpawnBlock(TRIANGLE);
+			break;
+		case 2:
+			SpawnBlock(CUBE);
+			break;
+		case 3:
+			SpawnBlock(S_BLOCK);
+			break;
+		case 4:
+			SpawnBlock(Z_BLOCK);
+			break;
+		case 5:
+			SpawnBlock(L_BLOCK);
+			break;
+		case 6:
+			SpawnBlock(RL_BLOCK);
+			break;
+		}
+	}
+
 	void MoveSideWays(const char& a)
 	{
-		if (y1 < 25 && y2 < 25 && y3 < 25 && y4 < 25) {
-			if (x1 > 0 && x2 > 0 && x3 > 0 && x4 > 0 && a < 0 || 
-				a > 0 && x1 < GAME_WIDTH - 1 && x2 < GAME_WIDTH - 1 && x3 < GAME_WIDTH - 1&& x4 < GAME_WIDTH - 1)
+		if (posY1 < 25 && posY2 < 25 && posY3 < 25 && posY4 < 25) {
+			if (posX1 > 0 && posX2 > 0 && posX3 > 0 && posX4 > 0 && a < 0 ||
+				a > 0 && posX1 < GAME_WIDTH - 1 && posX2 < GAME_WIDTH - 1 && posX3 < GAME_WIDTH - 1&& posX4 < GAME_WIDTH - 1)
 			{
-				data[y1] &= ~(0x1 << x1);
-				data[y2] &= ~(0x1 << x2);
-				data[y3] &= ~(0x1 << x3);
-				data[y4] &= ~(0x1 << x4);
+				data[posY1] &= ~(0x1 << posX1);
+				data[posY2] &= ~(0x1 << posX2);
+				data[posY3] &= ~(0x1 << posX3);
+				data[posY4] &= ~(0x1 << posX4);
 
-				x1 += a;
-				x2 += a;
-				x3 += a;
-				x4 += a;
+				posX1 += a;
+				posX2 += a;
+				posX3 += a;
+				posX4 += a;
 
-				data[y1] |= 0x1 << x1;
-				data[y2] |= 0x1 << x2;
-				data[y3] |= 0x1 << x3;
-				data[y4] |= 0x1 << x4;
+				data[posY1] |= 0x1 << posX1;
+				data[posY2] |= 0x1 << posX2;
+				data[posY3] |= 0x1 << posX3;
+				data[posY4] |= 0x1 << posX4;
 			}
 		}
+	}
+	
+	bool CanMoveSideWays(const char& a)
+	{
+		if (posX1 > 0 && posX2 > 0 && posX3 > 0 && posX4 > 0 && a < 0 ||
+			a > 0 && posX1 < GAME_WIDTH - 1 && posX2 < GAME_WIDTH - 1 && posX3 < GAME_WIDTH - 1 && posX4 < GAME_WIDTH - 1)
+		{
+			return true;
+		}
+
+		return false;
 	}
 
 	void Rotate(void)
 	{
 		char tmpX, tmpY;
 
-		data[y2] &= ~(0x1 << x2);
-		//tmpX = (x2 * cos(90)) - (y2 * sin(90));
-		//tmpY = (x2 * sin(90)) - (y2 * cos(90));
-		tmpY = y2 - y1;
-		tmpX = x2 - x1;
+		data[posY2] &= ~(0x1 << posX2);
 
-		x2 = x1 - tmpY;
-		y2 = y1 - tmpX;
+		tmpX = -(posY2 - posY1);
+		tmpY = (posX2 - posX1);
 
-		data[y3] &= ~(0x1 << x3);
-		tmpY = y3 - y1;
-		tmpX = x3 - x1;
+		posX2 = tmpX + posX1;
+		posY2 = tmpY + posY1;
 
-		x3 = x1 - tmpY;
-		y3 = y1 - tmpX;
+		data[posY3] &= ~(0x1 << posX3);
+		
+		tmpX = -(posY3 - posY1);
+		tmpY = (posX3 - posX1);
 
-		data[y4] &= ~(0x1 << x4);
-		tmpY = y4 - y1;
-		tmpX = x4 - x1;
+		posX3 = tmpX + posX1;
+		posY3 = tmpY + posY1;
 
-		x4 = x1 - tmpY;
-		y4 = y1 - tmpX;
+		data[posY4] &= ~(0x1 << posX4);
 
-		data[y1] |= 0x1 << x1;
-		data[y2] |= 0x1 << x2;
-		data[y3] |= 0x1 << x3;
-		data[y4] |= 0x1 << x4;
+		tmpX = -(posY4 - posY1);
+		tmpY = (posX4 - posX1);
+
+		posX4 = tmpX + posX1;
+		posY4 = tmpY + posY1;
+
+		data[posY1] |= 0x1 << posX1;
+		data[posY2] |= 0x1 << posX2;
+		data[posY3] |= 0x1 << posX3;
+		data[posY4] |= 0x1 << posX4;
 	}
 
-	void MoveDown(void)
+	int MoveDown(void)
 	{
-		if (y1 < 25 && y2 < 25 && y3 < 25 && y4 < 25 &&
-			CanMoveDown(x1, y1) &&
-			CanMoveDown(x2, y2) &&
-			CanMoveDown(x3, y3) &&
-			CanMoveDown(x4, y4))
+		if (posY1 < 25 && posY2 < 25 && posY3 < 25 && posY4 < 25 &&
+			CanMoveDown(posX1, posY1) &&
+			CanMoveDown(posX2, posY2) &&
+			CanMoveDown(posX3, posY3) &&
+			CanMoveDown(posX4, posY4))
 		{
-			data[y1] &= ~(0x1 << x1);
-			data[y2] &= ~(0x1 << x2);
-			data[y3] &= ~(0x1 << x3);
-			data[y4] &= ~(0x1 << x4);
+			data[posY1] &= ~(0x1 << posX1);
+			data[posY2] &= ~(0x1 << posX2);
+			data[posY3] &= ~(0x1 << posX3);
+			data[posY4] &= ~(0x1 << posX4);
 
-			y1++;
-			y2++;
-			y3++;
-			y4++;
+			posY1++;
+			posY2++;
+			posY3++;
+			posY4++;
 
-			data[y1] |= 0x1 << x1;
-			data[y2] |= 0x1 << x2;
-			data[y3] |= 0x1 << x3;
-			data[y4] |= 0x1 << x4;
+			data[posY1] |= 0x1 << posX1;
+			data[posY2] |= 0x1 << posX2;
+			data[posY3] |= 0x1 << posX3;
+			data[posY4] |= 0x1 << posX4;
 		}
 
 		else
 		{
-			switch (rand() % 4)
+			if (posY1 <= 2)
 			{
-			case 0:
-				SpawnBlock(LINE);
-				break;
-			case 1:
-				SpawnBlock(TRIANGLE);
-				break;
-			case 2:
-				SpawnBlock(CUBE);
-				break;
-			case 3:
-				SpawnBlock(S_BLOCK);
-				break;
+				return 0;
 			}
+
+			SpawnRandomBlock();
 		}
+
+		return 1;
 	}
 
-	bool CanMoveDown( const char& posX, const char& posY)
+	bool CanMoveDown(const char& posX, const char& posY)
 	{
 		if (data[posY + 1] & 0x1 << posX)
 		{
-			if (x1 == posX && posY + 1 == y1 || x2 == posX && posY + 1 == y2 || x3 == posX && posY + 1 == y3 || x4 == posX && posY + 1 == y4)
+			if (posX1 == posX && posY + 1 == posY1 || posX2 == posX && posY + 1 == posY2 || posX3 == posX && posY + 1 == posY3 || posX4 == posX && posY + 1 == posY4)
 			{
 				return true;
 			}
@@ -169,12 +216,11 @@ int main(void)
 {
 	system("color 9b");
 
-	char a = '0';
 	Block block;
-	block.SpawnBlock(TRIANGLE);
+	block.SpawnRandomBlock();
 
-	char index = 0;
-
+	char i;
+	char j;
 	do
 	{
 
@@ -183,7 +229,8 @@ int main(void)
 
 		if (index > 10) 
 		{
-			block.MoveDown();
+			if (!block.MoveDown())
+				break;
 			index = 0;
 		}
 
@@ -206,13 +253,12 @@ int main(void)
 		}
 
 		system("cls");
-		char i;
 		for (i = 0; i < 26; i++)
 		{
-			for (a = 0; a < 4; a++)
-			{
-				putchar(178);
-			}
+			putchar(219);
+			putchar(219);
+			putchar(219);
+			putchar(219);
 
 			for (a = 0; a < GAME_WIDTH; a++)
 			{
@@ -222,10 +268,10 @@ int main(void)
 					putchar(' ');
 			}
 
-			for (a = 0; a < 4; a++)
-			{
-				putchar(178);
-			}
+			putchar(219);
+			putchar(219);
+			putchar(219);
+			putchar(219);
 
 			if(i == 1)
 			{
@@ -234,12 +280,17 @@ int main(void)
 
 			if (i == 2)
 			{
-				fputs("  A/D = 左/右", stdout);
+				fputs("  S = 下", stdout);
 			}
 
 			if (i == 3)
 			{
-				fputs("  S = 下", stdout);
+				fputs("  A/D = 左/右", stdout);
+			}
+
+			if (i == 4)
+			{
+				fputs("  W = 回転", stdout);
 			}
 
 			putchar('\n');
@@ -248,27 +299,35 @@ int main(void)
 			if ((UINT32_MAX / 2) - data[i] <= 0)
 			{
 				data[i] = 0;
-				score += 100;
+				score += 10;
 
 				for (a = 25; a > 0; a--)
 				{
-					data[a] = data[a - 1];
+					for (j = 0; j < GAME_WIDTH; j++)
+					{
+						if (!(data[a] & 0x1 << j) && data[a - 1] & 0x1 << j && a != 0)
+						{
+							data[a] |= (data[a - 1] & 0x1 << j);
+							data[a - 1] &= ~(0x1 << j);
+
+							j = 0;
+						}
+					}
 				}
 			}
 		}
 
 		for (a = 0; a < 120; a++)
 		{
-			putchar(178);
+			putchar(219);
 		}
 
-		//printf("\n x1 = %d, x2 = %d, x3 = %d, x4 = %d \n data[25] = %u : INT_MAX = %u : Sum %u", block.x1, block.x2, block.x3, block.x4, data[25], UINT32_MAX/2, UINT32_MAX/2 - data[25]);
-		
-		index++;
+		++index;
 		fflush(stdout);
-		Sleep(10);
 
 	} while (true);
 
+	fputs("\n\nGAME OVER ", stdout);
+	getchar();
 	return 0;
 }
